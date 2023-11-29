4206718f3e7afdbb3b156c822354425d7adbc804
chiseDelta <ghostymyname@gmail.com>
2023-11-29 07:09:49 +0200

    array's and object's is done

---
+++ lesson2/main.js
@@ -0,0 +1,39 @@
+// Array's and Object's
+// const arr = [1, 2, 3, 4, true, false, 'bear', 'simple', {name: 'Max', status: true}, [1, 2, 3]];
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+// console.log(arr[4])
+// console.log(arr[5])
+// console.log(arr[6])
+// console.log(arr[7])
+// console.log(arr[8])
+// console.log(arr[9])
+
+// const book1 = {title: 'Title1', pageCount: 16, genre: false, authors: [{name: 'Max', age: 55}, {name: 'Lena', age: 50}]}
+// const book2 = {title: 'Title2', pageCount: 32, genre: true, authors: [{name: 'Sam', age: 21}, {name: 'Mia', age: 27}]}
+// const book3 = {title: 'Title3', pageCount: 64, genre: true, authors: [{name: 'Nick', age: 34}, {name: 'Oleg', age: 42}]}
+
+// const users = [
+//     {name: 'Max', username: 'max55', password: '55max55'},
+//     {name: 'Lena', username: 'lena50', password: '50lena50'},
+//     {name: 'Sam', username: 'sam21', password: '21sam21'},
+//     {name: 'Mia', username: 'mia27', password: '27mia27'},
+//     {name: 'Nick', username: 'nick34', password: '34nick34'},
+//     {name: 'Oleg', username: 'oleg42', password: '42oleg42'},
+//     {name: 'user1', username: 'userPro1', password: 'userProPassword1'},
+//     {name: 'user2', username: 'userPro2', password: 'userProPassword2'},
+//     {name: 'user3', username: 'userPro3', password: 'userProPassword3'},
+//     {name: 'user4', username: 'userPro4', password: 'userProPassword4'}
+// ]
+// console.log(users[0].password)
+// console.log(users[1].password)
+// console.log(users[2].password)
+// console.log(users[3].password)
+// console.log(users[4].password)
+// console.log(users[5].password)
+// console.log(users[6].password)
+// console.log(users[7].password)
+// console.log(users[8].password)
+// console.log(users[9].password)
