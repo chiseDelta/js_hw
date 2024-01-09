7c95e763e3d159886da8a6b9468cf806372d446b
chiseDelta <ghostymyname@gmail.com>
2024-01-09 14:16:23 +0200

    task5 is finished

---
+++ lesson10/task5/main.js
@@ -0,0 +1,14 @@
+// - Створити довільний елемент з id = text та створити кнопку. Використовуючи JavaScript, зробіть так, щоб при натисканні на кнопку зникав елемент з id="text".
+const h1 = document.createElement('h1');
+const button = document.createElement('button');
+
+h1.innerText = 'Hello World!';
+button.innerText = 'Delete World';
+
+document.body.append(h1, button);
+
+let buttonStatus = true;
+button.addEventListener('click', () => {
+    h1.style.color = buttonStatus ? 'white' : 'black';
+    buttonStatus = !buttonStatus
+})
